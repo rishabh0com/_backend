9420219901fc48a70fd3b5e5d2f38afc186c261b
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2023-12-06 22:51:46 +0530

    first commit Backend

---
+++ Practice/Day1-CJS/calc.js
@@ -0,0 +1,7 @@
+// importing sum function from index.js
+
+const func = require("./index.js")
+// const {sum , sub} = require("./index.js")
+
+console.log("addition is : ", func.sum(2,4))
+console.log("subtraction is :",func.sub(2,4));
---
+++ Practice/Day1-CJS/index.js
@@ -0,0 +1,14 @@
+
+// sum function
+const sum = (a,b) => {
+    return a + b;
+}
+
+const sub = (a,b) => {
+    return a - b;
+}
+
+
+
+// exporting sum
+module.exports = {sum,sub}
