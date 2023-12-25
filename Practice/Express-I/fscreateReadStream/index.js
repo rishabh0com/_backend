1d1d04fbc84a326d66e9eaffdd8e09753b56a34c
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2023-12-25 09:19:32 +0530

    added practice assignment

---
+++ Practice/Express-I/fscreateReadStream/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs")
+const http = require("http")
+
+const server = http.createServer((req,res)=>{
+    if(req.url == "/"){
+        res.end("Home Page ..")
+    }
+    else if(req.url == "/data")
+    {
+        let data = fs.createReadStream("./db.json",{encoding:"utf-8"})
+        data.pipe(res)
+        console.log(fs.readFileSync("./db.json",{encoding:"utf-8"}))
+    }
+    else{
+        res.end("404")
+    }
+})
+
+server.listen(8080,()=>{console.log("server is running")})
