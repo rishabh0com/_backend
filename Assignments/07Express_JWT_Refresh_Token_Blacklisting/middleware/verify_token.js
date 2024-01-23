9a95811bd76497ab247d4e352e79ef510922a684
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-01-23 13:48:57 +0530

    enble logout and check revoked token functionality

--- Assignments/07Express_JWT_Refresh_Token_Blacklisting/middleware/verify_token.js
+++ Assignments/07Express_JWT_Refresh_Token_Blacklisting/middleware/verify_token.js
@@ -1,10 +1,20 @@
 const jwt = require("jsonwebtoken");
+const { RevokedToken } = require("../model/Revoked_token.model");
 const dotenv = require("dotenv").config();
 
 const auth = async (req, res, next) => {
   try {
     let access_token = req.headers.authorization.split(" ")[0];
-    let refresh_token = req.headers.authorization.split(" ")[1];
+    let refresh_token = req.query.refreshToken;
+    const isTokenRevoked = await RevokedToken.findOne({ token: refresh_token });
+    if (isTokenRevoked) {
+      return res
+        .status(401)
+        .send({
+          message: "Refresh token is revoked",
+          revokedAt: isTokenRevoked.createdAt,
+        });
+    }
     console.log(access_token);
     jwt.verify(
       access_token,
@@ -18,14 +28,18 @@ const auth = async (req, res, next) => {
             process.env.refresh_secret,
             (err, decoded) => {
               if (!err) {
-                let new_access_token = jwt.sign({}, process.env.access_secret,{expiresIn:40});
+                let new_access_token = jwt.sign({}, process.env.access_secret, {
+                  expiresIn: 40,
+                });
                 res.send({ new_access_token });
               } else {
                 res.send("login again..");
               }
-            });
+            }
+          );
         }
-      });
+      }
+    );
   } catch (error) {
     console.log(error);
     res.status(400).send({ error });
--- Assignments/07Express_JWT_Refresh_Token_Blacklisting/routes/authRoutes.js
+++ Assignments/07Express_JWT_Refresh_Token_Blacklisting/routes/authRoutes.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const { UserModel } = require("../model/User.model")
 const bcrypt = require("bcrypt");
 const { auth } = require("../middleware/verify_token");
+const { RevokedToken } = require("../model/Revoked_token.model");
 const dotenv = require("dotenv").config();
 
 const authRoutes = express.Router();
@@ -88,5 +89,26 @@ authRoutes.post("/login", async (req, res) => {
 //     res.status(400).send({ error: error });
 //   }
 // });
+
+authRoutes.get("/logout", async (req, res) => {
+  try {
+    const refreshToken = req.query.refreshToken;
+
+    // Check if the refresh token is blacklisted
+    const revokedToken = await RevokedToken.findOne({ token: refreshToken });
+
+    if (revokedToken) {
+      return res.status(401).send({ message: 'Refresh token is revoked', revokedAt: revokedToken.createdAt });
+    }
+
+    // Blacklist the refresh token
+    await RevokedToken.create({ token: refreshToken });
+
+    res.status(200).send({ message: 'Logout successful' });
+  } catch (error) {
+    console.error('Error during logout:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+  }
+})
 
 module.exports = { authRoutes };
