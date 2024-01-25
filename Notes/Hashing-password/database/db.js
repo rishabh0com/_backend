a0166207f5e51df0742850a48afff3e8c149489c
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-01-25 09:53:12 +0530

    repo commit

--- Notes/Hashing-password/database/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv").config()
-
-const connection = mongoose.connect(process.env.mongoDBUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = { connection };
