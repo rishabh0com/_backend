32473a41d7d37cc204d95db1c2ecd815488bffd5
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-01-31 14:04:28 +0530

    added models

--- Assignments/09-Full_Stack_CRUD_Deployment/server/models/note.model.js
+++ Assignments/09-Full_Stack_CRUD_Deployment/server/models/note.model.js
@@ -1,18 +1,19 @@
 const mongooes = require("mongoose");
 const moment = require("moment");
 
-const todoSchema = mongooes.Schema(
+const noteSchema = mongooes.Schema(
   {
     title: { type: String, required: true },
-    status: { type: Boolean, required: true },
+    discription: { type: String, required: true },
     time: { type: String, default: moment().format("LT") },
     date: { type: Date, default: moment().format("L") },
+    userID: { type: String, required: true },
   },
   { versionKey: false }
 );
 
-const TodoModel = mongooes.model("todo", todoSchema);
+const NoteModel = mongooes.model("note", noteSchema);
 
-module.exports = { TodoModel };
+module.exports = { NoteModel };
 // console.log(moment().format('LT'))
 // console.log(moment().format('L'))
---
+++ Assignments/09-Full_Stack_CRUD_Deployment/server/models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    DOB: { type: String, required: true },
+    phone: { type: Number, required: false },
+  },
+  { versionKey: false }
+);
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = { UserModel };
