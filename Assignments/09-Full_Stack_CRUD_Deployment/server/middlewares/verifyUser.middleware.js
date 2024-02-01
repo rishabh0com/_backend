a3953f75e92bce3ca2cd44e03ef0936a519d48b6
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-01 12:16:50 +0530

    modify middleware

--- Assignments/09-Full_Stack_CRUD_Deployment/server/middlewares/verifyUser.middleware.js
+++ Assignments/09-Full_Stack_CRUD_Deployment/server/middlewares/verifyUser.middleware.js
@@ -3,8 +3,9 @@ const { TokenModel } = require("../models/revoked.model");
 require("dotenv").config();
 
 const authentication = async (req, res, next) => {
-  const { refreshToken } = req.require;
-  const accessToken = req.headers.authorization.split(" ")[1];
+  // console.log(req.cookies);
+  const { refreshToken, accessToken } = req.cookies;
+  // const accessToken = req.headers.authorization.split(" ")[1];
 
   try {
     if (!refreshToken && !accessToken) throw new Error("tokens are required");
@@ -12,18 +13,45 @@ const authentication = async (req, res, next) => {
     const isTokenBlacklisted = await TokenModel.findOne({
       token: refreshToken,
     });
-    if (isTokenBlacklisted) res.send({ msg: "login first" });
+    if (isTokenBlacklisted) throw new Error("login first");
 
-    const verifyAccss = jwt.verify(accessToken, process.env.secret_access);
-    if (verifyAccss) next();
-
-    const verifyRefresh = jwt.verify(refreshToken, process.env.secret_refresh);
-    if (verifyRefresh) {
-      const newAccessToken = jwt.sign({}, process.env.secret_access, {
-        expiresIn: "1h",
-      });
-      res.send({ msg: "new accessToken is generated", newAccessToken });
-    } else res.send({ msg: "login first" });
+    jwt.verify(accessToken, process.env.secret_access, (err, decoded) => {
+      if (decoded) {
+        // console.log(decoded);
+        next();
+      } else if (err.message == "jwt expired") {
+        const verifyRefresh = jwt.verify(
+          refreshToken,
+          process.env.secret_refresh
+        );
+        if (verifyRefresh) {
+          const newAccessToken = jwt.sign({}, process.env.secret_access, {
+            expiresIn: 60,
+          });
+          res.cookie("accessToken", newAccessToken);
+          res.send({ msg: "new accessToken is generated" });
+        } else res.send({ msg: "login first" });
+      }
+    });
+    // console.log(verifyAccss)
+    // if (verifyAccss) {
+    //   console.log(verifyAccss);
+    //   req.payload = verifyAccss;
+    //   next();
+    // } else {
+    //   console.log("In refresh")
+    //   const verifyRefresh = jwt.verify(
+    //     refreshToken,
+    //     process.env.secret_refresh
+    //   );
+    //   if (verifyRefresh) {
+    //     const newAccessToken = jwt.sign({}, process.env.secret_access, {
+    //       expiresIn: 60,
+    //     });
+    //     res.cookie("newAccessToken", newAccessToken);
+    //     res.send({ msg: "new accessToken is generated",newAccessToken });
+    //   } else res.send({ msg: "login first" });
+    // }
   } catch (error) {
     res.send({ msg: error.message, error });
   }
